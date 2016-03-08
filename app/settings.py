5bbfd5907c95e04efdfb2dc3cfa305708d2993d1
andela-jngatia <joan.ngatia@andela.com>
2016-03-08 08:25:50 +0300

    (Feature) Create required models.

--- app/settings.py
+++ app/settings.py
@@ -37,6 +37,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'bucketlist',
 ]
 
 MIDDLEWARE_CLASSES = [
@@ -123,3 +124,8 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/1.9/howto/static-files/
 
 STATIC_URL = '/static/'
+
+
+# Default user creation to Account model
+
+AUTH_USER_MODEL = 'bucketlist.Account'
--- bucketlist/models.py
+++ bucketlist/models.py
@@ -1,5 +1,66 @@
 from __future__ import unicode_literals
 
 from django.db import models
+from django.contrib.auth.models import AbstractBaseUser
 
-# Create your models here.
+
+class Account(AbstractBaseUser):
+    """Extend Django's built -in User model that inherits from AbstractUser."""
+
+    username = models.CharField(max_length=80, unique=True)
+    email = models.EmailField(unique=True)
+
+    tagline = models.CharField(max_length=140)
+
+    is_admin = models.BooleanField(default=False)
+
+    # specify the required field and default username field
+    USERNAME_FIELD = 'username'
+
+    def __unicode__(self):
+        """Display string representation of username."""
+        return self.username
+
+
+class Base(models.Model):
+    """Create common base class for bucketlist and item creation."""
+
+    name = models.CharField(max_length=250)
+
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
+
+    class Meta:
+        abstract = True
+        ordering = ['updated_at']
+
+
+class Bucketlist(Base):
+    """Extend and customize base model."""
+
+    creator = models.ForeignKey(Account)
+
+    class Meta(Base.Meta):
+        """Extend parent meta class."""
+
+        db_table = 'bucketlist'
+
+    def __unicode__(self):
+        """Display string representation of bucketlistname."""
+        return self.name
+
+
+class Bucketlistitem(Base):
+    """Extend and customize base bucektlistitem model."""
+
+    done = models.BooleanField(default=False)
+    bucketlist = models.ForeignKey(Bucketlist)
+
+    class Meta(Base.Meta):
+        """Extend parent meta class."""
+
+        db_table = 'Bucketlistitem'
+
+    def __unicode__(self):
+        """Display string representation of bucketlistitemname."""
+        return self.name
