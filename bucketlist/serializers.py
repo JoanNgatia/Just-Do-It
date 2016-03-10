02478dff2f9e2e830182af715d1997763c12ee59
andela-jngatia <joan.ngatia@andela.com>
2016-03-10 22:43:24 +0300

    (Chore) Clean up Seerializer code.

--- bucketlist/serializers.py
+++ bucketlist/serializers.py
@@ -1,5 +1,43 @@
+from django.contrib.auth import update_session_auth_hash
+
 from rest_framework import serializers
-from models import Bucketlist, Bucketlistitem
+from models import Account, Bucketlist, Bucketlistitem
+
+
+class AccountSerializer(serializers.ModelSerializer):
+    """Define User serialization fields."""
+
+    # define that password can be changed only if need be
+    password = serializers.CharField(write_only=True, required=False)
+    confirm_password = serializers.CharField(write_only=True, required=False)
+
+    class Meta:
+        """Define metadata the serializer should use."""
+
+        model = Account
+        fields = ('id', 'username', 'created_at', 'updated_at',
+                  'tagline', 'password', 'confirm_password',)
+        read_only_fields = ('created_at', 'updated_at',)
+
+    def create(self, **validated_data):
+        """Deserialize json and create a new user."""
+        return Account.objects.create(validated_data)
+
+    def update(self, instance, **validated_data):
+        """Deserialize json and update user details."""
+        instance.tagline = validated_data.get('tagline', instance.tagline)
+
+        password = validated_data.get('password', None)
+        confirm_password = validated_data.get('confirm_password', None)
+
+        # incase of password update check that both fields have been filled
+        if password and confirm_password and password == confirm_password:
+            instance.set_password(password)
+            instance.save()
+        # save updated password to current session to avoid re-logging in
+        update_session_auth_hash(self.context.get('request'), instance)
+
+        return instance
 
 
 class BucketlistitemSerializer(serializers.ModelSerializer):
@@ -7,9 +45,20 @@ class BucketlistitemSerializer(serializers.ModelSerializer):
 
     class Meta:
         model = Bucketlistitem
-        fields = ('name', 'done', 'bucketlist')
-
+        fields = ('id', 'name', 'done', 'bucketlist',
+                  'created_at', 'updated_at')
         read_only_fields = ('created_at', 'updated_at')
+
+    def create(self, **validated_data):
+        return Bucketlistitem.objects.create(validated_data)
+
+    def update(self, instance, **validated_data):
+        instance.name = validated_data.get('name', instance.name)
+        instance.done = validated_data.get('done', instance.done)
+
+        instance.save()
+
+        return instance
 
 
 class BucketlistSerializer(serializers.ModelSerializer):
@@ -19,6 +68,14 @@ class BucketlistSerializer(serializers.ModelSerializer):
 
     class Meta:
         model = Bucketlist
-        fields = ('name', 'creator', 'items')
+        fields = ('id', 'name', 'creator', 'created_at', 'updated_at''items')
 
         read_only_fields = ('created_at', 'updated_at')
+
+    def create(self, **validated_data):
+        return Bucketlist.objects.create(validated_data)
+
+    def update(self, instance, **validated_data):
+        instance.name = validated_data.get('name', instance.name)
+
+        return instance
