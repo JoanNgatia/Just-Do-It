ccfa3778881d9148af0bedc36beab4b98d352e08
andela-jngatia <joan.ngatia@andela.com>
2016-03-13 00:46:38 +0300

    (Chore) Add URL endpoints for bucketlist items

--- bucketlist/urls.py
+++ bucketlist/urls.py
@@ -5,8 +5,12 @@ from . import views
 urlpatterns = [
     url(r'^users/$', views.AccountsList.as_view()),
     url(r'^users/(?P<pk>[0-9]+)/$', views.AccountsDetail.as_view()),
-    url(r'^bucketlists/$', views.BucketList.as_view()),
+    url(r'^bucketlists/$', views.BucketListView.as_view()),
     url(r'^bucketlists/(?P<pk>[0-9]+)/$', views.BucketlistDetail.as_view()),
+    url(r'^bucketlists/(?P<pk>[0-9]+)/items/$',
+        views.BucketlistItemView.as_view()),
+    url(r'^bucketlists/(?P<pk>[0-9]+)/items/(?P<pk_item>[0-9]+)/$',
+        views.BucketlistItemDetail.as_view()),
 ]
 
 urlpatterns = format_suffix_patterns(urlpatterns)
