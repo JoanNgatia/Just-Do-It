b4d6e9079741e8108c6099f3408bac6c5b096f66
andela-jngatia <joan.ngatia@andela.com>
2016-03-12 23:26:10 +0300

    (Chore) Impose owner-only access to bucketlists.

--- bucketlist/permissions.py
+++ bucketlist/permissions.py
@@ -1,11 +1,15 @@
 from rest_framework import permissions
 
 
-class IsAccountOwner(permissions.BasePermission):
+class IsOwnerOrReadOnly(permissions.BasePermission):
     """Define account permissions."""
 
     def has_object_permission(self, request, view, account):
-        """Check user associated with request is same object as account."""
-        if request.user:
-            return account == request.user
-        return False
+        """Check user associated with request is same object as account.
+
+        Read permissions allowed to any request.
+        """
+        if request.method in permissions.SAFE_METHODS:
+            return True
+
+        return account.creator == request.user
