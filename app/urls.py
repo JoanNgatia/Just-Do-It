0eff9b5f75c7e6f6c64671511dc1e464f6103375
andela-jngatia <joan.ngatia@andela.com>
2016-03-21 21:39:02 +0300

    [Chore] Add reverse lookup reference for token URL

--- app/urls.py
+++ app/urls.py
@@ -24,5 +24,5 @@ urlpatterns = [
     url(r'^api/auth/',
         include('rest_framework.urls', namespace='rest_framework')),
     url(r'^docs/', include('rest_framework_swagger.urls')),
-    url(r'^api/token/', views.obtain_auth_token),
+    url(r'^api/token/', views.obtain_auth_token, name='token'),
 ]
