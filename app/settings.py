0ddd4df6ee3b747fff1b6cfc991a850834013be1
andela-jngatia <joan.ngatia@andela.com>
2016-03-22 09:40:08 +0300

    [Chore] Add django-nose for test coverage

--- app/settings.py
+++ app/settings.py
@@ -42,8 +42,18 @@ INSTALLED_APPS = [
     'rest_framework',
     'rest_framework_swagger',
     'rest_framework.authtoken',
+    'django_nose',
     # internal apps
     'bucketlist',
+]
+
+# Use nose to run all tests
+TEST_RUNNER = 'django_nose.NoseTestSuiteRunner'
+
+# Tell nose to measure coverage on the 'foo' and 'bar' apps
+NOSE_ARGS = [
+    '--with-coverage',
+    '--cover-package=bucketlist',
 ]
 
 MIDDLEWARE_CLASSES = [
