23e43c465e7411e875d0b1a8f73174201d6e6863
andela-jngatia <joan.ngatia@andela.com>
2016-04-03 00:14:23 +0300

    [Feature] Enable Creation of new bucketlists

--- bucketlist/views/bucketlist_view.py
+++ bucketlist/views/bucketlist_view.py
@@ -5,12 +5,14 @@ from django.contrib import messages
 from django.template import RequestContext
 
 from bucketlist.models import Bucketlist
+from bucketlist.forms.forms_bucketlist import BucketListForm
 
 
 class AllBucketlistsView(TemplateView):
     """View to handle retrieval and creation of bucketlists."""
 
     template_name = 'bucketlists/buckets.html'
+    form_class = BucketListForm
 
     def get_context_data(self, **kwargs):
         """Return dictionary representing passed in context."""
@@ -18,4 +20,26 @@ class AllBucketlistsView(TemplateView):
         context['buckets'] = Bucketlist.objects.filter(
             creator=self.request.user)
         context['username'] = self.request.user.username
+        context['bucketlistform'] = BucketListForm()
         return context
+
+    def post(self, request, **kwargs):
+        """Method to create a new bucketlist."""
+        form = self.form_class(request.POST)
+        if form.is_valid():
+            new_bucket = form.save()
+            new_bucket.creator = self.request.user
+            new_bucket.save()
+            messages.success(
+                request, 'New Bucketlist added successfully!')
+            return redirect(
+                '/bucketlist',
+                context_instance=RequestContext(request)
+            )
+        else:
+            messages.error(
+                request, 'Error at creation!')
+            return redirect(
+                '/bucketlist',
+                context_instance=RequestContext(request)
+            )
