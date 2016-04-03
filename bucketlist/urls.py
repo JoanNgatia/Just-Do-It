69b439bbf68d5311eeb4d3212f239e27bab3bef4
andela-jngatia <joan.ngatia@andela.com>
2016-04-04 02:59:41 +0300

    [Feature] Add URL for viewing of all bucketlists

--- bucketlist/urls.py
+++ bucketlist/urls.py
@@ -1,14 +1,14 @@
 from django.conf.urls import url
 from rest_framework.urlpatterns import format_suffix_patterns
-from . import views
-from bucketlist.views import authentication_view
+from bucketlist.views import authentication_view, bucketlist_view
 
 urlpatterns = [
-    # url(r'^$', views.index, name='dashboard'),
     url(r'^$', authentication_view.IndexView.as_view(), name='index'),
     url(r'^register$', authentication_view.RegistrationView.as_view(),
         name="register"),
     url(r'^login$', authentication_view.LoginView.as_view(), name="login"),
+    url(r'^bucketlists', bucketlist_view.AllBucketlistsView.as_view(),
+        name="all_bucketlists"),
 ]
 
 urlpatterns = format_suffix_patterns(urlpatterns)
