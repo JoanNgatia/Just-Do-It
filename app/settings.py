3070bf3f486b13c16fda2665c7b45d1db782d8b9
andela-jngatia <joan.ngatia@andela.com>
2016-04-05 09:52:34 +0300

    [Chore] Reconfigure staticfiles root

--- app/settings.py
+++ app/settings.py
@@ -145,11 +145,11 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/1.9/howto/static-files/
 STATIC_ROOT = os.path.join(BASE_DIR, 'static')
-STATIC_URL = '/bucketlist/static/'
+STATIC_URL = '/static/'
+STATIC_ROOT = 'staticfiles'
 
 
 # Default user creation to Account model
-
 AUTH_USER_MODEL = 'bucketlist.Account'
 
 REST_FRAMEWORK = {
