1e7c2f729b889ac82e6c87cd9279fe6ef819c35d
andela-jngatia <joan.ngatia@andela.com>
2016-04-06 01:41:58 +0300

    [Feature] Add URL for edition of bucketlists

--- bucketlist/urls.py
+++ bucketlist/urls.py
@@ -7,8 +7,11 @@ urlpatterns = [
     url(r'^register$', authentication_view.RegistrationView.as_view(),
         name="register"),
     url(r'^login$', authentication_view.LoginView.as_view(), name="login"),
-    url(r'^bucketlists', bucketlist_view.AllBucketlistsView.as_view(),
+    url(r'^bucketlists/$', bucketlist_view.AllBucketlistsView.as_view(),
         name="all_bucketlists"),
+    url(r'^bucketlists/(?P<pk>[0-9]+)/$',
+        bucketlist_view.BucketlistDetailView.as_view(),
+        name="single_bucketlist"),
 ]
 
 urlpatterns = format_suffix_patterns(urlpatterns)
