7f35fe34abac6a3e19691bb75605276106090d7c
andela-jngatia <joan.ngatia@andela.com>
2016-04-07 15:47:22 +0300

    [Chore] Restyle authentication forms

--- bucketlist/forms/forms_authentication.py
+++ bucketlist/forms/forms_authentication.py
@@ -1,10 +1,7 @@
-import re
 from django import forms
 from django.core.exceptions import ObjectDoesNotExist
 
 from bucketlist.models import Account
-# from .models import Account
-# from django.utils.translation import ugettext_lazy as _
 
 
 class RegistrationForm(forms.Form):
@@ -12,15 +9,14 @@ class RegistrationForm(forms.Form):
     username = forms.RegexField(regex=r'^[0-9a-zA-Z_]*$', max_length=30,
                                 widget=forms.TextInput(attrs=dict(
                                     required=True,
-                                    render_value=False)),
-                                label='Username')
+                                    render_value=False)), label='Username')
     password = forms.CharField(widget=forms.PasswordInput(attrs=dict(
-        required=True, render_value=False)), label='Password')
+        required=True, render_value=False)), label='')
     confirm_password = forms.CharField(widget=forms.PasswordInput(
-        attrs=dict(required=True, render_value=False)),
-        label='Confirm password')
-    tagline = forms.CharField(widget=forms.TextInput,
-                              label='Favorite Catchphrase')
+        attrs=dict(required=True,
+                   render_value=False)), label='')
+    tagline = forms.CharField(widget=forms.TextInput(
+        attrs=dict(required=True)), label='')
 
     def clean_username(self):
         try:
@@ -31,7 +27,7 @@ class RegistrationForm(forms.Form):
 
     def clean(self):
         try:
-            if self.cleaned_data['password'] != self.cleaned_data['verify_password']:
+            if self.cleaned_data['password'] != self.cleaned_data['confirm_password']:
                 raise forms.ValidationError("Passwords do not match")
         except KeyError:
             return self.cleaned_data
@@ -46,12 +42,8 @@ class RegistrationForm(forms.Form):
 
 class LoginForm(forms.Form):
     username = forms.CharField(widget=forms.TextInput(attrs=dict({
-        'placeholder': 'Enter unique username',
         'autocomplete': 'off'
     })))
     password = forms.CharField(
-        label='Password', max_length=100,
-        widget=forms.PasswordInput(
-            attrs={
-                'placeholder': '**********'
-            }))
+        label='', max_length=100,
+        widget=forms.PasswordInput)
