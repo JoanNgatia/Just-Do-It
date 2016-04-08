77df5fdb996adf463ae1d6b50a69f63257cf3a50
andela-jngatia <joan.ngatia@andela.com>
2016-04-08 15:36:23 +0300

    [Feature] Create form for creation of bucketlist items

--- bucketlist/forms/forms_bucketlist.py
+++ bucketlist/forms/forms_bucketlist.py
@@ -1,9 +1,25 @@
 from django import forms
 
-from bucketlist.models import Bucketlist
+from bucketlist.models import Bucketlist, Bucketlistitem
 
 
 class BucketListForm(forms.ModelForm):
+    """Form for creation of a bucketlist.
+
+    Extends from bucketlistmodel.
+    """
+
     class Meta:
         model = Bucketlist
+        fields = ['name']
+
+
+class BucketlistItemForm(forms.ModelForm):
+    """Form for creation of a bucketlistitem.
+
+    Extends from bucketlistitem model.
+    """
+
+    class Meta:
+        model = Bucketlistitem
         fields = ['name']
