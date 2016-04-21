843152df5770383bebb7514eedc34d88e63ce244
andela-jngatia <joan.ngatia@andela.com>
2016-04-21 15:59:00 +0300

    [Chore] Restyle landing page

--- bucketlist/static/js/main.js
+++ bucketlist/static/js/main.js
@@ -8,15 +8,6 @@ $.ajax({
 });
 
 var main = function() {
-
-    $('#login').click(function(){
-        $('#loginModal').modal('show');
-    });
-
-    $('#register').click(function(){
-        $('#registerModal').modal('show');
-    });
-
     $('#all_bucketlists').click(function(){
         $('#newbucketModal').modal('show');
     });
@@ -27,18 +18,12 @@ var main = function() {
 
     $('[data-toggle="tooltip"]').tooltip({animation: true, delay: {show: 300, hide: 300}});
 
-    $("#flash-message").fadeOut(7000);
+    $(".flash-message").fadeOut(7000);
 }
 
 $(document).ready(main);
 
-/* Toggle between adding and removing the "active" and "show" classes when the user clicks on one of the "Section" buttons. The "active" class is used to add a background color to the current button when its belonging panel is open. The "show" class is used to open the specific accordion panel */
-var acc = document.getElementsByClassName("accordion");
-var i;
 
-for (i = 0; i < acc.length; i++) {
-    acc[i].onclick = function(){
-        this.classList.toggle("active");
-        this.nextElementSibling.classList.toggle("show");
-    }
-}
+$('.message a').click(function(){
+   $('form').animate({height: "toggle", opacity: "toggle"}, "slow");
+});
